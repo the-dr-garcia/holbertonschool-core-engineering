87c992a50463e896c171aa60f43a4343707e9256
Daniel Garcia <daniel.ivan.garcia.aguilar@gmail.com>
2026-09-07 00:18:04 +0200

    Add common_elements.py script

---
+++ python_fundamentals/core_data_structures/common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that returns common elements between two sets."""
+
+
+def common_elements(set_1, set_2):
+    """Return a new set containing elements present in both sets."""
+    return set_1 & set_2
