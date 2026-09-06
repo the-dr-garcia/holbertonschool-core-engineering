36489b32f9f5e7c36e1af6598864c014cb2546c8
Daniel Garcia <daniel.ivan.garcia.aguilar@gmail.com>
2026-09-07 00:04:05 +0200

    Add calculation.py script

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Import functions from calculator_1 and print results."""
+"""Print results of arithmetic operations."""
 from calculator_1 import add, sub, mul, div
 
 a = 10
