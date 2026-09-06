c31dcba01c5cdbf3433f253a13ea5c96175a0237
Daniel Garcia <daniel.ivan.garcia.aguilar@gmail.com>
2026-09-07 00:02:14 +0200

    Add calculation.py script

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -6,7 +6,5 @@ a = 10
 b = 5
 
 if __name__ == "__main__":
-    print(add(a, b))
-    print(sub(a, b))
-    print(mul(a, b))
-    print(div(a, b))
+    print("{}\n{}\n{}\n{}".format(add(a, b), sub(a, b),
+                                   mul(a, b), div(a, b)))
