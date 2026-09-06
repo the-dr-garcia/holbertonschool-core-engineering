ca4e5a1d489098e157adac432259c23f2261c189
Daniel Garcia <daniel.ivan.garcia.aguilar@gmail.com>
2026-09-06 23:15:53 +0200

    Add print_last_digit.py script

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -7,5 +7,5 @@ def print_last_digit(number):
     last_digit = number % 10
     if last_digit < 0:
         last_digit += 10
-    print("{}".format(last_digit))
+    print("{:d}".format(last_digit), end="")
     return last_digit
