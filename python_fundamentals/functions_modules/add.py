332a823b7b238f41901c62b7c7658caba9891f34
Daniel Garcia <daniel.ivan.garcia.aguilar@gmail.com>
2026-09-06 23:30:08 +0200

    Add add.py script

--- python_fundamentals/functions_modules/add.py
+++ python_fundamentals/functions_modules/add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Module for adding two numbers using an imported function."""
 
 from add_0 import add
 
@@ -6,4 +7,4 @@ from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    print(f"{a} + {b} = {add(a, b)}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
