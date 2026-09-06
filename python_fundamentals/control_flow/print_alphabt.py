b7507620aa7752090b5279f40e5fcd743500e4d5
Daniel Garcia <daniel.ivan.garcia.aguilar@gmail.com>
2026-09-06 22:30:16 +0200

    Add print_alphabt.py script

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -3,5 +3,5 @@ result = ""
 for i in range(97, 123):
     letter = chr(i)
     if letter != 'q' and letter != 'e':
-        result += letter
+        result = "{}{}".format(result, letter)
 print(result)
