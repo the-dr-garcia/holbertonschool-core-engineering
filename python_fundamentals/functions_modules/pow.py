94efcfc8894f029a98433e6f57161e612ce5a1da
Daniel Garcia <daniel.ivan.garcia.aguilar@gmail.com>
2026-09-06 23:21:12 +0200

    Add pow.py script

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -5,6 +5,11 @@
 def pow(a, b):
     """Return a raised to the power of b using a manual loop."""
     result = 1
-    for i in range(b):
-        result *= a
+    if b >= 0:
+        for i in range(b):
+            result *= a
+    else:
+        for i in range(-b):
+            result *= a
+        result = 1 / result
     return result
