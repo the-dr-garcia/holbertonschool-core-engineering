92524646e85a1eb2da76ae56e8fd8323ecfd3feb
Daniel Garcia <daniel.ivan.garcia.aguilar@gmail.com>
2026-09-06 23:12:23 +0200

    Add uppercase.py script

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module that defines a function to print a string in uppercase."""
+
+
+def uppercase(str):
+    """Print the given string converted to uppercase using ASCII logic."""
+    result = ""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            result += chr(ord(c) - 32)
+        else:
+            result += c
+    print(result)
