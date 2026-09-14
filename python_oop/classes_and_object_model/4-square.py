4c6a4d3728204b8a7fb1fb8255dcd1248c1b1fdb
Daniel Garcia <daniel.ivan.garcia.aguilar@gmail.com>
2026-09-14 04:43:14 +0200

    Add 4-square.py script

---
+++ python_oop/classes_and_object_model/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the current size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return self.__size ** 2
