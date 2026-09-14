f6f0c45e6136e969cce17f77d5cac06546243c66
Daniel Garcia <daniel.ivan.garcia.aguilar@gmail.com>
2026-09-14 04:58:52 +0200

    Add base_geometry.py script

---
+++ python_oop/inheritance/base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """A class representing base geometry."""
+
+    def area(self):
+        """Raises an Exception indicating area is not implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates a value as a positive integer.
+
+        Args:
+            name (str): The name of the value.
+            value (int): The value to validate.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
