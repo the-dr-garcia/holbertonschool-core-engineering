8c2c1694d02008403d49302db5a3992703933518
Daniel Garcia <daniel.ivan.garcia.aguilar@gmail.com>
2026-09-14 04:41:16 +0200

    Add 3-square.py script

---
+++ python_oop/classes_and_object_model/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current area of the square."""
+        return self.__size ** 2
